d98528ceb4ce17b1ec986203020fc569663a66d4
Bitinho <victorcarneiro2009@gmail.com>
2018-05-09 22:38:43 -0300

    Created text drawing functions. All other graphics functions tested and working. On #7 - item 3 and 4 done.

--- src/Graphics.py
+++ src/Graphics.py
@@ -2,6 +2,8 @@ import pygame
 from Board import *
 import glob
 import math
+import sys
+from Constants import *
 
 pygame.font.init()
 
@@ -48,6 +50,7 @@ class Graphics:
         pygame.display.update()
         self.clock.tick(self.fps)
 
+
     def draw_message(self, message):
         """
         Draws message to the screen.
@@ -84,7 +87,8 @@ class AnimatedGraphic(Graphic):
     looping = None
 
     def __init__(self, path, frameDelay, loop=False):
-        paths = glob.glob(path)
+        paths = sorted(glob.glob(path))
+        print(paths)
         self.surfaces = list(map(pygame.image.load, paths))
         self.frameDelay = frameDelay
         self.frameDelayCounter = frameDelay
@@ -101,7 +105,7 @@ class AnimatedGraphic(Graphic):
                 self.frame += 1
                 if self.frame >= len(self.surfaces):
                     self.frame = 0
-                self.surface = self.surfaces(self.frame)
+                self.surface = self.surfaces[self.frame]
 
 
 class Motion:
@@ -143,6 +147,7 @@ class Motion:
         self.currentSpeed = 0.0
         self.nodeCompletion = 1.0
         self.currentPos = self.coord2
+        self.path.pop(0)
 
     def update(self, timeDelta=1.0 / 60):
         """Updates the movement coordinates."""
@@ -171,9 +176,11 @@ class Motion:
             self.hasCompleted = True
             self.nodeCompletion = 1.0
         else:
-            node = self.path.pop[0]
+            node = self.path.pop(0)
             self.coord2 = node.coords
-            self.currentSpeed = node.speed / euclideanDist(self.coord1, self.coord2)
+            if(self.coord1 == self.coord2): self.currentSpeed = 1.0
+            else: 
+                self.currentSpeed = node.speed / euclideanDist(self.coord1, self.coord2)
             self.accelerate = node.accelerate
             self.decelerate = node.decelerate
             self.nextEvent = node.eventOnComplete
@@ -208,28 +215,128 @@ class EasingMotion(Motion):
            quadratic interpolation and the accelerate/decelerate flags."""
         # Case 1: acceleration only
         if self.accelerate and not self.decelerate:
-            return self.coord2 * (self.nodeCompletion ** 2) + self.coord1 * (1.0 - (self.nodeCompletion ** 2))
+            return (self.coord2[0] * (self.nodeCompletion ** 2) + 
+                    self.coord1[0] * (1.0 - (self.nodeCompletion ** 2)),
+                    self.coord2[1] * (self.nodeCompletion ** 2) + 
+                    self.coord1[1] * (1.0 - (self.nodeCompletion ** 2)))
+
 
         # Case 2: deceleration only
         if self.decelerate and not self.accelerate:
-            return self.coord2 * (1.0 - ((1.0 - self.nodeCompletion) ** 2)) + self.coord1 * (
-                    (1.0 - self.nodeCompletion) ** 2)
+            return (self.coord2[0] * (1.0 - ((1.0 - self.nodeCompletion) ** 2))
+                  + self.coord1[0] * ((1.0 - self.nodeCompletion) ** 2), 
+                    self.coord2[1] * (1.0 - ((1.0 - self.nodeCompletion) ** 2))
+                  + self.coord1[1] * ((1.0 - self.nodeCompletion) ** 2))
+
 
             # Case 3: acceleration and deceleration
         if self.decelerate and self.accelerate:
             # This function is decomposed into two quadratic curves. 1st half:
             if (self.nodeCompletion < 0.5):
-                return self.coord2 * (2.0 * self.nodeCompletion ** 2) / 2.0 + self.coord1 * (
-                        1.0 - ((2.0 * self.nodeCompletion) ** 2) / 2.0)
+                return (self.coord2[0] * (2.0 * self.nodeCompletion ** 2) / 2.0 + 
+                        self.coord1[0] * (1.0 - ((2.0 * self.nodeCompletion) ** 2) / 2.0),
+                        self.coord2[1] * (2.0 * self.nodeCompletion ** 2) / 2.0 + 
+                        self.coord1[1] * (1.0 - ((2.0 * self.nodeCompletion) ** 2) / 2.0))
+
             # 2nd half:
             else:
-                return self.coord2 * (1.0 - (2.0 * (1.0 - self.nodeCompletion) ** 2 / 2.0)) + self.coord1 * (
-                        2.0 * (1.0 - self.nodeCompletion) ** 2 / 2.0)
+                return (self.coord2[0] * (1.0 - (2.0 * (1.0 - self.nodeCompletion) ** 2 / 2.0))
+                      + self.coord1[0] * (2.0 * (1.0 - self.nodeCompletion) ** 2 / 2.0), 
+                        self.coord2[1] * (1.0 - (2.0 * (1.0 - self.nodeCompletion) ** 2 / 2.0))
+                      + self.coord1[1] * (2.0 * (1.0 - self.nodeCompletion) ** 2 / 2.0))
+
 
         # Case 4: linear motion (not accelerated or decelerated)
         return Motion.calcPosition(self)
 
+class TextElement:
+    fontFace = None
+    alignment = None
+    text = None
+    surface = None
+    color = None
+    coords = None
+    
+    def __init__(self, text, coords, fontFace="../assets/fonts/Cantarell-Regular.otf", fontSize=36, alignment=FONT_ALIGN_LEFT, color=pygame.Color(255, 255, 255, 255)):
+        if(alignment < 0 or alignment > 2): raise RuntimeError("TextElement @__init__: Invalid alignment type `{}'".format(alignment))
+        self.fontFace = pygame.font.Font(fontFace, fontSize)
+        self.alignment = alignment
+        self.text = text
+        self.color = color
+        self.coords = coords
+        self.render()
+
+    def blitAt(self, surface, coords=None):
+        if coords is None: coords = self.coords 
+        surface.blit(self.surface, self.getSurfaceOrigin(coords))
+
+    def render(self):
+        self.surface = self.fontFace.render(self.text, True, self.color)
+
+    def getSurfaceOrigin(self, coords):
+        if(self.alignment == FONT_ALIGN_LEFT):
+            return (coords[0], coords[1] - self.fontFace.get_linesize())
+        elif(self.alignment == FONT_ALIGN_RIGHT):
+            return (coords[0] - self.surface.get_width(), coords[1] - self.fontFace.get_ascent())
+        else:
+            return (coords[0] - (self.surface.get_width() / 2), coords[1] - self.fontFace.get_ascent())
+
+    def update(self, value=None, newCoords=None):
+        if newCoords is tuple: self.coords = newCoords
+
+class DynamicTextElement(TextElement):
+    def __init__(self, initialValue, coords, fontFace="../assets/fonts/Cantarell-Regular.otf", fontSize=36, alignment=FONT_ALIGN_LEFT, color=pygame.Color(255, 255, 255, 255)):
+        TextElement.__init__(self, initialValue, coords, fontFace, fontSize, alignment, color)
+
+    def update(self, newValue, newCoords=None):
+        if newValue != self.text:
+            self.text = newValue
+            self.render()
+        if newCoords is tuple: self.coords = newCoords
 
 def euclideanDist(coord1, coord2):
     """Returns the euclidean distance of two coordinates - (x, y) tuples"""
     return math.sqrt((coord2[0] - coord1[0]) ** 2 + (coord2[1] - coord1[1]) ** 2)
+
+def main():
+    graphics = Graphics()
+    graphics.setupWindow()
+    text = TextElement("Hey! Listen!", (100, 100))
+    count = DynamicTextElement("0", (500, 100), fontFace="../assets/fonts/NimbusSansNarrow-Bold.otf", fontSize=50, alignment=FONT_ALIGN_CENTER)
+    graphic = Graphic("../assets/images/piece-white.png")
+    graphic2 = Graphic("../assets/images/piece-red.png")
+    animation1 = AnimatedGraphic("../assets/images/highlight-possible_spot_marker-f*", 1, loop=True)
+    animation2 = AnimatedGraphic("../assets/images/highlight-possible_spot_marker-f*", 2, loop=True)
+    motion = EasingMotion([PathNode((232, 368), 0), PathNode((300, 300), 650, pygame.event.Event(pygame.USEREVENT), accelerate=True), PathNode((368, 232), 650, None, decelerate=True)])
+    text.blitAt(graphics.screen)
+    pygame.display.update()
+    pygame.display.flip()
+    counter = 0
+    pieceDestroyed = False
+    while(True):
+        timeDelta = graphics.clock.tick(graphics.fps) / 1000.0
+        counter += 1
+        count.update(str(counter))
+        animation1.update()
+        animation2.update()
+        motion.update(timeDelta)
+
+        graphics.screen.fill((0, 0, 0))
+        text.blitAt(graphics.screen)
+        count.blitAt(graphics.screen)
+        if not pieceDestroyed: graphic.blitAt(graphics.screen, (300, 300))
+        graphic2.blitAt(graphics.screen, motion.currentPos)
+        animation1.blitAt(graphics.screen, (300, 500))
+        animation2.blitAt(graphics.screen, (400, 500))
+
+        pygame.display.update()
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                pygame.quit()
+                sys.exit()
+            if(event.type == pygame.USEREVENT):
+                pieceDestroyed = True
+            
+
+if __name__ == "__main__": main()
