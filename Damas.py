5360eb2210c38131a14bf2ff362b120c8d4255a6
Bitinho <victorcarneiro2009@gmail.com>
2018-06-09 11:37:02 -0300

    Fixed a potential issue in the startup script.

--- Damas.py
+++ Damas.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+import sys
+import os
 try:
     import pygame
 except:
     print("Erro: este aplicativo precisa da biblioteca externa `pygame' para ser executado.\nPor favor, instale-a antes de executar este programa.")
     sys.exit(1)
-import sys
-import os
 sys.path.append(os.path.abspath("src"))
 os.chdir(os.path.abspath("src"))
 import Title
