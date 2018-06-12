846e7c469f05061f83efc642654543d1cb15b31d
Bitinho <victorcarneiro2009@gmail.com>
2018-06-12 17:27:03 -0300

    Implemented test_gamehover and test_pausehover. Closes #38.

---
+++ src/test_pausehover.py
@@ -0,0 +1,57 @@
+from GameLoop import *
+
+class FakeAIPlayer(AI.AIPlayer):
+    def __init__(self):
+        pass
+
+    def play(self):
+        return
+
+class TestingGameLoop(GameLoop):
+    def __init__(self, graphicsBackend, difficultyLevel, playerName, boardDesc):
+        GameLoop.__init__(self, graphicsBackend, difficultyLevel, playerName)
+        self.board = Board(boardDesc)
+        self.graphics = Graphics(graphicsBackend, self.board, playerName)
+        self.aiPlayer = FakeAIPlayer()
+
+    def mainLoop(self):
+        self.state = "pause"
+        while self.state == "pause" or self.state == "anim":
+            self.states[self.state]()
+            self.updateMainGame()
+            if self.exitedGame: return
+
+    def pauseEventLoop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.terminateGame()
+
+            elif event.type == pygame.MOUSEMOTION:
+                self.updateMousePos(event.pos)
+
+boardList = [
+        [
+        "#r#r#r#r",
+        "r#r#r#r#",
+        "#r#r#r#r",
+        " # # # #",
+        "# # # # ",
+        "w#w#w#w#",
+        "#w#w#w#w",
+        "w#w#w#w#"
+        ],
+]
+
+def main():
+    graphicsBackend = GraphicsBackend()
+    for board in boardList:
+        game = TestingGameLoop(graphicsBackend, 2, "", board).mainLoop()
+        clearScreen(graphicsBackend)
+    print("\n\nAll tests done. Exiting.")
+        
+def clearScreen(graphicsBackend):
+    graphicsBackend.screen.fill(pygame.Color(0, 0, 0))
+    pygame.display.flip()
+    graphicsBackend.clock.tick(5)
+
+if __name__ == "__main__": main()
